1149013d9c25637edc55331e699019536b63005d
KristianCebic <kristian.cebic13@gmail.com>
2024-05-14 18:40:29 +0200

    update

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
@@ -8,7 +8,33 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
+
 export class PortfolioComponent {
+  @ViewChildren('project') elements!: QueryList<ElementRef>; //holt alle elemente, die beobachtet werden sollen
+  
+  constructor () {
+
+    const options = { //einstellungen, die festlegen, ab welchem bereich ein element als "beobachtet" festgelegt werden soll
+      root: null,
+      rootMargin: '0px',
+      threshold: .4
+    }
+ 
+    const callbacks = (entries: IntersectionObserverEntry[]) => { //festlegt, was passieren soll, wenn ein intersection observer eine callback funktion aufruft
+      entries.forEach((entry: IntersectionObserverEntry) => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('animationSlideInLeft');
+          }
+      });
+  };
+  
+  let observer = new IntersectionObserver(callbacks, options); // erstellt eine neue instanz für den observer
+
+  this.elements.forEach((element: ElementRef) => { // erstellt eine beobachtung für jeden einzelnen dom element
+    observer.observe(element.nativeElement);
+  });
+  }
+
   liveTest(url: string) {
     window.open(url, '_blank');
   }
